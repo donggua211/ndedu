a517ddb416828847fc37b0651ab9acf0334dbf10
zhaoyuan211@gmail.com <zhaoyuan211@gmail.com@cea4be19-621a-fb77-3afb-6c200120d319>
2011-11-01 07:09:14 +0000

    -make ndedu1.2.6 path

--- application/language/cn/admin_lang.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-$lang['username_password_empty'] = '您输入的用户名或者密码为空，请重新输入!';
-$lang['username_password_wrong'] = '您输入的用户名或者密码有误，请重新输入!';
-$lang['password_wrong'] = '您输入的密码有误，请重新输入!';
-$lang['passwords_not_match'] = '您输入的新密码不一致，请重新输入!';
-$lang['password_empty'] = '您的密码不能为空，请重新输入!';
-$lang['passwords_changed'] = '密码已经修改成功!';
-
-?>
