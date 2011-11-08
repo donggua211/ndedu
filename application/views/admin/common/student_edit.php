067ebc924de6f89406d27003e5377a2ccb261a22
zhaoyuan211@gmail.com <zhaoyuan211@gmail.com@cea4be19-621a-fb77-3afb-6c200120d319>
2011-11-08 10:11:41 +0000

    -update 1.2.6.1

--- application/views/admin/common/student_edit.php
+++ application/views/admin/common/student_edit.php
@@ -32,6 +32,24 @@
 			</tr>
 			<?php endif; ?>
 			
+			
+			<?php if($CI->admin_ac_student->view_student_edit_jiaowu($student['status'])): ?>
+			<tr>
+				<td class="label" valign="top"><span class="notice-star"> * </span>教务老师: </td>
+				<td>
+					<select name="jiaowu_id">
+						<option value='0'>请选择...</option>
+						<?php
+							foreach($jiaowus as $jiaowu)
+							{
+								echo '<option value="'.$jiaowu['staff_id'].'" '.((isset($student['jiaowu_id']) && $jiaowu['staff_id'] == $student['jiaowu_id']) ? 'SELECTED' : '' ).'>'.$jiaowu['name'].'</option>';
+							}
+						?>
+					</select>
+				</td>
+			</tr>
+			<?php endif; ?>
+			
 			<?php if($CI->admin_ac_student->view_student_edit_consultant($student['status'])): ?>
 			<tr>
 				<td class="label" valign="top"><span class="notice-star"> * </span>咨询师: </td>
