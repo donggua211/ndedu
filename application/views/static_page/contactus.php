db3c3f2103bfba26b50b46a90c287049a16b3ad2
zhaoyuan211 <zhaoyuan211@cea4be19-621a-fb77-3afb-6c200120d319>
2012-11-10 05:27:43 +0000

    -update

--- application/views/static_page/contactus.php
+++ application/views/static_page/contactus.php
@@ -7,20 +7,9 @@
 	</div>
 	<div class="right">
 		<div class="contactus">
-			<div class="font_16_bold color_lg">健康服务投诉：</div>
-			<div class="mail">
-				<p>电话：010-85627897</p>
-				<p>邮箱：gaish@gmail.com</p>
-			</div>
-			<div class="font_16_bold color_o">尚康产品投诉：</div>
-			<div class="mail">
-				<p>电话：010-85627897</p>
-				<p>邮箱：gaish@gmail.com</p>
-			</div>
-			<div><span class="font_16_bold color_lb">尚康产品合作：</div>
 			<div class="boys">
-				<p>电话：010-85627897</p>
-				<p>邮箱：gaish@gmail.com</p>
+				<p>电话：010-59790750</p>
+				<p>邮箱：51915411@qq.com</p>
 			</div>
 		</div>
 	</div>
